fde1aa8752dd0541ea901cf87fce6c0a22671cd0
xdkomel <xdkomel@users.noreply.github.com>
2024-07-04 06:23:48 +0000

    deploy: 1a056f5945c409ea333534518ba234cab0b09cac

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4274904778"
+    serviceWorkerVersion: "2236149009"
   }
 });
